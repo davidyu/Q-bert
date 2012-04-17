0ac76a2b8f85c61ced0350de8d13308f08e85920
David Yu <lewen.yu@gmail.com>
2012-04-17 00:12:52 -0400

    Cube rendering method Added openGL methods for rendering a cube given position and dimensions

--- include/ENTITY_cQube.hpp
+++ include/ENTITY_cQube.hpp
@@ -2,20 +2,35 @@
 #define CQUBE_H
 
 #include <vector>
-typedef struct Color
-{
-    float r;
-    float g;
-    float b;
-    float a;
-} Color;
+#include <SDL2/SDL_opengl.h>
+#include "Color.hpp"
 
 namespace ENTITY
 {
     class cQube
     {
         public:
-                        cQube(int x, int y, int z, int w, int l, int h, Color top, Color faces);
+            cQube(int x, int y, int z, int w, int d, int h, Color up, Color rest);
+            cQube(int x, int y, int z, int w, int d, int h, Color up, Color down, Color left, Color right, Color front, Color back);
+
+            void render();
+
+        protected:
+
+        int _x,
+            _y,
+            _z;
+
+        int _width,
+            _depth,
+            _height;
+
+        Color _up,
+              _down,
+              _left,
+              _right,
+              _front,
+              _back;
     };
 }
 
---
+++ src/Color.cpp
@@ -0,0 +1,20 @@
+#include "Color.hpp"
+
+Color::Color(float r, float g, float b, float a)
+{
+    _r = r;
+    _g = g;
+    _b = b;
+    _a = a;
+}
+
+Color::Color(const Color& copy)
+:_r(copy.getR()), _g(copy.getG()), _b(copy.getB()), _a(copy.getA())
+{
+
+}
+
+Color::~Color()
+{
+    //dtor
+}
