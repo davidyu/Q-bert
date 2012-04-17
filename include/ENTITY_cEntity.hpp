30281edc0d2b632606ef8c3675d2cffcb8e25c40
David Yu <lewen.yu@gmail.com>
2012-04-17 01:54:29 -0400

    added cEntity base class for entities; kept in a vector from which render() is called

---
+++ include/ENTITY_cEntity.hpp
@@ -0,0 +1,16 @@
+#ifndef CENTITY_H
+#define CENTITY_H
+
+namespace ENTITY
+{
+    class cEntity
+    {
+        public:
+            cEntity();
+            virtual ~cEntity();
+            virtual void render();
+
+    };
+}
+
+#endif CENTITY_H
---
+++ src/ENTITY_cEntity.cpp
@@ -0,0 +1,7 @@
+#include "ENTITY_cEntity.hpp"
+
+using namespace ENTITY;
+
+cEntity::cEntity() {}
+cEntity::~cEntity() {}
+void cEntity::render() {}
